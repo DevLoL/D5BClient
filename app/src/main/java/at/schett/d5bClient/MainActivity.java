d8d8c1ceae683cf5111e16fc997312cf91f0adb1
Matthias Schett <matt.schett@gmail.com>
2014-03-01 14:09:27 +0100

    Made some changes to the application handling

--- app/src/main/java/at/schett/d5bClient/MainActivity.java
+++ app/src/main/java/at/schett/d5bClient/MainActivity.java
@@ -3,60 +3,47 @@ package at.schett.d5bClient;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
-import at.schett.d5bClient.util.SystemUiHider;
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class MainActivity extends Activity {
 
-    private Button scanBtn;
-    private TextView formatTxt, contentTxt;
+    private Button btnScan;
+    private TextView contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        scanBtn = (Button)findViewById(R.id.scan_button);
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        contentTxt = (TextView)findViewById(R.id.scan_content);
-        scanBtn.setY(scanBtn.getY() + 500);
-        //scanBtn.setOnClickListener(this);
-        IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-        scanIntegrator.initiateScan();
+        btnScan = (Button) findViewById(R.id.scan_button);
+        contentTxt = (TextView) findViewById(R.id.scan_content);
     }
-/*
-    @Override
-    public void onClick(View view) {
-        if(view.getId()==R.id.scan_button){
-            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-            scanIntegrator.initiateScan();
+
+    public void scanNow(View view) {
+        if (view.getId() == R.id.scan_button) {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
         }
     }
-*/
+
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (scanningResult != null) {
-            String scanContent = scanningResult.getContents();
-            String scanFormat = scanningResult.getFormatName();
-            formatTxt.setText("FORMAT: " + scanFormat);
-            contentTxt.setText("CONTENT: " + scanContent);
-        }else{
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "No scan data received!", Toast.LENGTH_SHORT);
-            toast.show();
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                Log.i("xZing", "contents: " + contents);
+                // Handle successful scan
+                contentTxt.setText(contents);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "No scan data received!", Toast.LENGTH_SHORT);
+                toast.show();
+                Log.i("xZing", "Cancelled");
+            }
         }
     }
 }
